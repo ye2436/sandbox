640f98fd84a5a2609339db832770972a807d1f2c
ye2436 <ye2436@gmail.com>
2016-12-22 17:22:04 -0600

    Add median of two sorted arrays

--- java-code/src/MedianOfTwoSortedArrays.java
+++ java-code/src/MedianOfTwoSortedArrays.java
@@ -8,12 +8,100 @@ public class MedianOfTwoSortedArrays {
      *记得数据结构那本书就说个把两个排序数组合并为一个排序数据的问题。
      *就是同时遍历两个数组，每次取两个数组开头小的那个放入新的数组，时间复杂度为 O(m+n)。
      *解法一，直接遍历，复杂度O(m+n)
+     * Merge Sort
      */
-    public double findMedianSortedArrays_1(int[] nums1, int[] nums2) {
+    public static double findMedianSortedArrays_1(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
+        int i = 0;
+        int j = 0;
+        int curr = 0;
+        int prev = 0;
+        double median;
 
-        return 0d;
+        if (m+n < 2) {
+            if (m==0 && n==0) {
+                return 0d;
+            } else if (m==1) {
+                return nums1[0];
+            } else {
+                return nums2[0];
+            }
+        }
+
+        // if m+n is even, then median is the avg of (m+n)/2 - 1 and (m+n)/2. (starting from 0)
+        // if m+n is odd, then median is (m+n-1)/2. (starting from 0). or just the integer part of (m+n)/2
+        while (i+j <= (m+n)/2) {
+            prev = curr;
+            if (i >= m) { // nums1 has run out
+                curr = nums2[j];
+                j++;
+            } else if (j >= n) { // nums2 has run out
+                curr = nums1[i];
+                i++;
+            } else if (nums1[i] < nums2[j]) {
+                curr = nums1[i];
+                i++;
+            } else {
+                curr = nums2[j];
+                j++;
+            }
+        }
+
+        if ((m+n)%2 == 0) {
+            median = (prev + curr) / 2.0;
+        } else {
+            median = curr;
+        }
+
+        return median;
+    }
+
+    public static double findMedianSortedArrays_2(int[] nums1, int[] nums2) {
+        double median;
+        int m = nums1.length;
+        int n = nums2.length;
+        if ((m+n)%2 == 0) { // find kth, starting from 1
+            median = (findKth(nums1,0,nums2,0,(m+n)/2) + findKth(nums1,0,nums2,0,(m+n)/2+1)) / 2.0;
+        } else {
+            median = findKth(nums1,0,nums2,0,(m+n)/2+1);
+        }
+
+        return median;
+    }
+
+    private static int findKth(int[] nums1, int start1,int[] nums2, int start2, int k) {
+
+        // if nums1 runs out, find the kth number in nums2 from start point 2.
+        // likewise if nums2 runs out.
+        if (start1 >= nums1.length) {
+            return nums2[start2 + k - 1];
+        } else if (start2 >= nums2.length) {
+            return nums1[start1 + k - 1];
+        }
+
+        // if k==1 return the smaller one of nums1 and nums2
+        if (k==1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+
+        // divide by half and keep finding kth/2
+        if (nums1[start1 + k/2 - 1] < nums2[start2 + k/2 - 1]) {
+            return findKth(nums1,start1+k/2, nums2,start2,k-k/2); // note that it is k-k/2, not k/2
+        } else {
+            return findKth(nums1,start1,nums2,start2+k/2, k-k/2);
+        }
+    }
+
+    public static void main(String[] args) {
+        //int[] nums1 = new int[]{1,3};
+        //int[] nums2 = new int[]{2,4};
+
+        int[] nums1 = new int[]{1};
+        int[] nums2 = new int[]{2,3,4,5,6};
+
+        System.out.println(findMedianSortedArrays_1(nums1, nums2));
+        System.out.println(findMedianSortedArrays_2(nums1, nums2));
     }
 
 
