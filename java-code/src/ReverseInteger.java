777ed52fdcd1d31f6f3d44ecab96dd64541fda96
yechen <ye2436@gmail.com>
2017-01-04 02:02:14 -0600

    Reverse Integer

---
+++ java-code/src/ReverseInteger.java
@@ -0,0 +1,16 @@
+/**
+ * Reverse digits of an integer.
+
+ Example1: x = 123, return 321
+ Example2: x = -123, return -321
+ */
+public class ReverseInteger {
+    public static int reverse(int x) {
+        return x;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse(123));
+    }
+}
