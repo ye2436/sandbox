194feb9b5ad4dab244d4a0273970f516c6a4fcc3
yechen <ye2436@gmail.com>
2017-01-11 00:05:40 -0600

    Merge K Sorted Lists

--- java-code/src/MergeKSortedLists.java
+++ java-code/src/MergeKSortedLists.java
@@ -1,19 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
  */
 public class MergeKSortedLists {
     public static ListNode mergeKLists(ListNode[] lists) {
-        return null;
+        ListNode head = new ListNode(0);
+
+
+
+        return head.next;
     }
 
 
-
     public static void main(String[] args) {
-        ListNode l1 = getListNode(5,false);
-        ListNode l2 = getListNode(5,true);
+        ListNode l1 = getListNode(5, 1);
+        ListNode l2 = getListNode(5, 2);
+        ListNode l3 = getListNode(5, 3);
+        ListNode l4 = getListNode(5, 4);
         System.out.println(print(l1));
         System.out.println(print(l2));
-        System.out.println(print(mergeKLists(new ListNode[]{l1, l2})));
+        System.out.println(print(l3));
+        System.out.println(print(l4));
+        System.out.println(print(mergeKLists(new ListNode[]{l1, l2, l3, l4})));
+
+        // test
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(0,1);
+        System.out.println(list);
+        System.out.println(2 >> 1);
+        System.out.println(3 >> 1);
+        System.out.println(4 >> 1);
     }
 
 
@@ -28,11 +47,10 @@ public class MergeKSortedLists {
     }
 
     /* helper */
-    public static ListNode getListNode(int n, boolean even) {
+    public static ListNode getListNode(int n, int factor) {
         ListNode head = new ListNode(0);
         ListNode current = head;
-        int i = even ? 2 : 1;
-        for (; i<=n*2; i+=2) {
+        for (int i=factor; i<=n*factor; i+=factor) {
             current.next = new ListNode(i);
             current = current.next;
         }
