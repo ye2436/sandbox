8cb9084547ac45ae124d843f672d7875421ed9a9
yechen <ye2436@gmail.com>
2017-01-18 23:32:19 -0600

    First Missing Positive Trapping Rain Water Multiply Strings

--- java-code/src/FirstMissingPositive.java
+++ java-code/src/FirstMissingPositive.java
@@ -8,7 +8,34 @@ import java.util.Arrays;
  *  Your algorithm should run in O(n) time and uses constant space.
  */
 public class FirstMissingPositive {
+    // use array's index to record number. let nums[i] = i+1
     public static int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) return 1;
+
+        // move nums[i] to nums[nums[i]-1]
+        for (int i=0; i<nums.length; i++) {
+            // only record positive numbers. ignore numbers that are larger than nums.length.
+            // skip nums that are already in position to avoid dead loops
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i]-1] != nums[i]) {
+                // swap it to position nums[i]-1
+                int temp = nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] = temp;
+                i--; // stay in the current position so the newly swapped in number will be processed
+            }
+        }
+
+        // find first number that doesn't meet nums[i] = i+1
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+
+        return nums.length+1;
+    }
+
+    public static int mySolution(int[] nums) {
         if (nums == null || nums.length == 0) return 1;
         Arrays.sort(nums);
         boolean positive = false;
---
+++ java-code/src/MultiplyStrings.java
@@ -0,0 +1,17 @@
+/**
+ * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+ * Note:
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contains only digits 0-9.
+ * Both num1 and num2 does not contain any leading zero.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ */
+public class MultiplyStrings {
+    public static String multiply(String num1, String num2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply("", ""));
+    }
+}
