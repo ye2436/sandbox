08b2493c7019a1a7e45004dcab861013ed048c0a
yechen <ye2436@gmail.com>
2017-01-25 00:59:32 -0600

    Permutation Sequence Rotate List Unique Paths

--- java-code/src/PermutationSequence.java
+++ java-code/src/PermutationSequence.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * The set [1,2,3,…,n] contains a total of n! unique permutations.
@@ -16,40 +17,57 @@ import java.util.List;
  */
 public class PermutationSequence {
     public static String getPermutation(int n, int k) {
-        // given an ordered array of [1,2,3,…,n], the calculated permutations by using backtracking
-        // will always be in ascending order. use k to track count, return only the kth.
-        char[] nums = new char[n];
+        StringBuilder sb = new StringBuilder();
+        List<Integer> nums = new ArrayList<>();
         for (int i=0; i<n; i++) {
-            nums[i] = (char) (i+1+'0');
+            nums.add(i+1);
         }
-        List<String> res = new ArrayList<>();
-        helper(nums, new StringBuilder(), res);
-        if (k-1<res.size()) {
-            return res.get(k-1);
+
+        int[] factorial = new int[n+1];
+        factorial[0] = 1;
+        for (int i=1; i<=n; i++) {
+            factorial[i] = factorial[i-1]*i;
         }
-        return null;
+
+        k--; // index starts from 0
+        int count=n;
+        while (count>0) {
+            int index = k / factorial[n-1];
+            sb.append(nums.get(index));
+            nums.remove(index); // O(n)
+            k = k % factorial[n-1];
+            n--;
+            count--;
+        }
+
+        return sb.toString();
     }
 
-    private static boolean helper(char[] nums, List<Character> currList, int count) {
+
+    // TLE
+    // given an ordered array of [1,2,3,…,n], the calculated permutations by using backtracking
+    // will always be in ascending order. use k to track count, return only the kth.
+    private static void helper(char[] nums, List<Character> currList, int count, List<String> res) {
         if (currList.size() == nums.length) {
             count--;
         }
         if (count == 0) {
-            return true;
+            res.add(currList.stream().map(e->e.toString()).collect(Collectors.joining()));
+            return;
+        } else if (count<0) {
+            return;
         }
         for (int i=0; i<nums.length; i++) {
             if (currList.contains(nums[i])) continue;
 
             currList.add(nums[i]);
-            if (helper(nums, currList, count)) {
-
-            }
+            helper(nums, currList, count,res);
             currList.remove(currList.size()-1);
         }
-        return false;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(getPermutation(3,5));
+        System.out.println(getPermutation(4,11));
     }
 }
