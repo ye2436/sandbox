b57eb04fe07716be6b2fe73dda82c67e8ec2a2f1
ye2436 <ye2436@gmail.com>
2017-01-25 17:16:06 -0600

    Unique Paths

--- java-code/src/UniquePaths.java
+++ java-code/src/UniquePaths.java
@@ -6,9 +6,27 @@
  * Note: m and n will be at most 100.
  */
 public class UniquePaths {
+    // dynamic programming
     public static int uniquePaths(int m, int n) {
-        return 0;
+        if (m<=0 || n<=0) return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        // dp[i][j] = dp[i][j-1] + dp[i-1][j]
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (j>0) {
+                    dp[i][j] += dp[i][j-1];
+                }
+                if (i>0) {
+                    dp[i][j] += dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
     }
+
+    // m+n-2 choose m-1, let m be the smaller one
 
     public static void main(String[] args) {
         System.out.println(uniquePaths(3,7));
