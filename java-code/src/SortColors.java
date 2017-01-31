62deec81f1989be9121549b05031f38b9072fd3d
ye2436 <ye2436@gmail.com>
2017-01-31 17:17:09 -0600

    Sort Colors Minimum Window Substring

--- java-code/src/SortColors.java
+++ java-code/src/SortColors.java
@@ -11,15 +11,53 @@ import java.util.Arrays;
  * Could you come up with an one-pass algorithm using only constant space?
  */
 public class SortColors {
+    // counting sort
     public static void sortColors(int[] nums) {
         if (nums == null || nums.length == 0) return;
+        int zero = 0;
+        int one = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                zero++;
+            } else if (num == 1) {
+                one++;
+            }
+        }
+        int i=0;
+        for (; i<zero; i++) {
+            nums[i] = 0;
+        }
+        for (; i<one+zero; i++) {
+            nums[i] = 1;
+        }
+        for (; i<nums.length; i++) {
+            nums[i] = 2;
+        }
+    }
 
-
+    // two pointers
+    public static void sortColors2(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int idx0 = 0;
+        int idx1 = 0;
+        for (int i=0;i<nums.length;i++) {
+            // the last element of all processed so far (nums[i]) would be 2
+            // if there is no 2's in curr list, idx0 and idx1 would push to the right and override it
+            // and then writes 1, and 0 in order. so non-existent 1's could be overwritten.
+            if (nums[i] == 0) {
+                nums[i] = 2;
+                nums[idx1++] = 1;
+                nums[idx0++] = 0;
+            } else if (nums[i] == 1) {
+                nums[i] = 2;
+                nums[idx1++] = 1;
+            } // do nothing if 2
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,3,5,7,8};
-        sortColors(nums);
+        int[] nums = {1,0,0,2,1,2,0,1};
+        sortColors2(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
