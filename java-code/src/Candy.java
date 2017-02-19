33718a905472d0dc260ce7ed5ba4f71f8d350fe2
yechen <ye2436@gmail.com>
2017-02-19 01:53:19 -0600

    Surrounded Regions, Palindrome Partitioning, Palindrome Partitioning II. Clone Graph, Gas Station, Candy.

---
+++ java-code/src/Candy.java
@@ -0,0 +1,18 @@
+/**
+ * #135. Candy
+ * There are N children standing in a line. Each child is assigned a rating value.
+ * You are giving candies to these children subjected to the following requirements:
+ * Each child must have at least one candy.
+ * Children with a higher rating get more candies than their neighbors.
+ * What is the minimum candies you must give?
+ */
+public class Candy {
+    public static int candy(int[] ratings) {
+        return 0;
+    }
+
+    public static void main (String[] args) {
+        int[] ratings = {};
+        System.out.println(candy(ratings));
+    }
+}
--- java-code/src/SurroundedRegions.java
+++ java-code/src/SurroundedRegions.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * #130. Surrounded Regions
@@ -29,6 +30,62 @@ public class SurroundedRegions {
     // 下面代码用的是广度优先搜索，用一个队列来维护，当然也可以用深度优先搜索，但是如果使用递归，会发现LeetCode过不了，
     // 这是因为在图形中通常图片（或者说这里的矩阵）一般会很大，递归很容易导致栈溢出，所以即使要用深度优先搜索，也最好使用非递归的实现方式
     public static void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) return;
+        for (int i=0; i<board.length; i++) { // left & right
+            fill(board, i,0);
+            fill(board, i, board[0].length-1);
+        }
+        for (int j=0; j<board[0].length; j++) { // top & bottom
+            fill(board, 0, j);
+            fill(board, board.length-1, j);
+        }
+        for (int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    // flood fill from start point (i,j), replace O with #
+    private static void fill(char[][] board, int i, int j) {
+        // if not equals to target, return
+        if (board[i][j] != 'O') {
+            return;
+        }
+        // replace the point and add to queue;
+        board[i][j] = '#';
+        LinkedList<int[]> queue = new LinkedList(); // int[]: row index and column index
+        queue.add(new int[]{i,j});
+        while (!queue.isEmpty()) {
+            int[] coordinates = queue.pop();
+            int row = coordinates[0];
+            int column = coordinates[1];
+
+            if (row > 0 && board[row-1][column] == 'O') { // up
+                board[row-1][column] = '#';
+                queue.add(new int[]{row-1, column});
+            }
+
+            if (row < board.length-1 && board[row+1][column] == 'O') { // down
+                board[row+1][column] = '#';
+                queue.add(new int[]{row+1, column});
+            }
+
+            if (column > 0 && board[row][column-1] == 'O') { // left
+                board[row][column-1] = '#';
+                queue.add(new int[]{row, column-1});
+            }
+
+            if (column < board[0].length-1 && board[row][column+1] == 'O') { // right
+                board[row][column+1] = '#';
+                queue.add(new int[]{row, column+1});
+            }
+        }
 
     }
 
