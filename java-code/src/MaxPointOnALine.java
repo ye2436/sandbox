3a4fb6187ee8ef4ded88cca862e8c90101e09669
yechen <ye2436@gmail.com>
2017-02-22 17:35:54 -0600

    Max Point on a Line, Evaluate Reverse Polish Notation, Reverse Words in a String, Maximum Product Subarray.

--- java-code/src/MaxPointOnALine.java
+++ java-code/src/MaxPointOnALine.java
@@ -1,5 +1,5 @@
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,41 +12,57 @@ public class MaxPointOnALine {
     public static int maxPoints(Point[] points) {
         if (points == null || points.length == 0) return 0;
 
-        // if y = ax + b
-        // a = y/x;
-        // b = y-ax;
-        Map<Integer, Map<Integer, Integer>> count = new HashMap<>(); // a -> b -> count
-        int max = 0;
-        for (Point point : points) {
-            int a = point.y / point.x;
-            int b = point.y - a * point.x;
-            if (!count.containsKey(a)) {
-                count.put(a, new HashMap<>());
+        int max = 1; // there is at least 1 point on a line
+        for (int i=0; i<points.length-1; i++) {
+            Map<BigDecimal, Integer> count = new HashMap<>(); // count based on slope
+            int countOfSamePoint = 0; // count of points that are same as points[i] (excludes points[i])
+            BigDecimal slope;
+            int maxStartHere = 1; // max starting from point[i]
+            for (int j=i+1; j<points.length; j++) {
+                if (points[j].x == points[i].x && points[j].y == points[i].y) {
+                    countOfSamePoint++;
+                    continue;
+                }
+                if (points[j].x == points[i].x) {
+                    slope = BigDecimal.valueOf(Integer.MAX_VALUE);
+                } else if (points[j].y == points[i].y) {
+                    slope = BigDecimal.ZERO;
+                } else {
+                    slope = BigDecimal.valueOf(points[j].y - points[i].y).divide(BigDecimal.valueOf(points[j].x - points[i].x), 16, RoundingMode.HALF_UP);
+                }
+                if (!count.containsKey(slope)) {
+                    count.put(slope, 1);
+                }
+                count.put(slope, count.get(slope)+1);
+                maxStartHere = Math.max(maxStartHere, count.get(slope));
             }
-            if (!count.get(a).containsKey(b)) {
-                count.get(a).put(b, 0);
-            }
-            count.get(a).put(b, count.get(a).get(b)+1);
-            max = Math.max(max, count.get(a).get(b));
+            max = Math.max(max, maxStartHere+countOfSamePoint);
         }
-
         return max;
     }
 
     public static void main(String[] args) {
+        System.out.println(94911150d/94911151d);
+        System.out.println(94911151d/94911152d);
+        System.out.println(BigDecimal.valueOf(94911150d).divide(BigDecimal.valueOf(94911151d), 16, RoundingMode.HALF_UP).toString());
+        System.out.println(BigDecimal.valueOf(94911151d).divide(BigDecimal.valueOf(94911152d), 16, RoundingMode.HALF_UP).toString());
         Point[] pArray = getPointArray();
         System.out.println(maxPoints(pArray));
     }
 
     private static Point[] getPointArray() {
         // y = 3x-1
-        Point p1 = new Point(1,2);
+        /*Point p1 = new Point(1,2);
         Point p2 = new Point(2,5);
         Point p3 = new Point(3,8);
-
         Point p4 = new Point(4,4);
+        return new Point[]{p1,p2,p3,p4};*/
 
-        return new Point[]{p1,p2,p3,p4};
+        // [[0,0],[94911151,94911150],[94911152,94911151]]
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(94911151,94911150);
+        Point p3 = new Point(94911152,94911151);
+        return new Point[]{p1,p2,p3};
     }
 
     private static class Point {
