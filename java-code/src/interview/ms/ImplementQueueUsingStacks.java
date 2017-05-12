7cfd8df2ca135f8f4546dbb18fb5f5d252ba7678
ye2436 <ye2436@gmail.com>
2017-05-12 17:14:38 -0500

    Misc ms

--- java-code/src/interview/ms/ImplementQueueUsingStacks.java
+++ java-code/src/interview/ms/ImplementQueueUsingStacks.java
@@ -1,5 +1,7 @@
 package interview.ms;
 
+import java.util.Stack;
+
 /**
  * 232. Implement Queue using Stacks
  * Implement the following operations of a queue using stacks.
@@ -18,31 +20,78 @@ public class ImplementQueueUsingStacks {
 
     // http://www.geeksforgeeks.org/queue-set-1introduction-and-array-implementation/
     // http://www.geeksforgeeks.org/queue-using-stacks/
+
+    // Method 1 (By making enQueue operation costly) This method makes sure that oldest entered element is always
+    // at the top of stack 1, so that deQueue operation just pops from stack1. To put the element at top of stack1, stack2 is used.
+    // enQueue(q, x)
+    // 1) While stack1 is not empty, push everything from stack1 to stack2.
+    // 2) Push x to stack1 (assuming size of stacks is unlimited).
+    // 3) Push everything back to stack1.
+    // dnQueue(q)
+    // 1) If stack1 is empty then error
+    // 2) Pop an item from stack1 and return it
+
+    // Method 2 (By making deQueue operation costly)
+    // In this method, in en-queue operation, the new element is entered at the top of stack1.
+    // In de-queue operation, if stack2 is empty then all the elements are moved to stack2 and finally top of stack2 is returned.
+    // enQueue(q,  x)
+    // 1) Push x to stack1 (assuming size of stacks is unlimited).
+    // deQueue(q)
+    // 1) If both stacks are empty then error.
+    // 2) If stack2 is empty
+    //      While stack1 is not empty, push everything from stack1 to stack2.
+    // 3) Pop the element from stack2 and return it.
+    //
+    // Method 2 is definitely better than method 1.
+    // Method 1 moves all the elements twice in enQueue operation, while method 2 (in deQueue operation)
+    // moves the elements once and moves elements only if stack2 empty.
+
     public class MyQueue {
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
 
         /** Initialize your data structure here. */
         public MyQueue() {
-
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
         }
 
         /** Push element x to the back of queue. */
         public void push(int x) {
-
+            stack1.push(x);
         }
 
         /** Removes the element from in front of queue and returns that element. */
         public int pop() {
-            return 0;
+            if (stack1.empty() && stack2.empty()) {
+                // throw new Exception
+            }
+            if (!stack2.empty()) {
+                return stack2.pop();
+            }
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
         }
 
         /** Get the front element. */
         public int peek() {
-            return 0;
+            if (stack1.empty() && stack2.empty()) {
+                // throw new Exception
+            }
+            if (!stack2.empty()) {
+                return stack2.peek();
+            }
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
         }
 
         /** Returns whether the queue is empty. */
         public boolean empty() {
-            return true;
+            return stack1.empty() && stack2.empty();
         }
     }
 
